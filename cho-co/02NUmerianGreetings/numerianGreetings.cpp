357ff1c18ef82570ff3ba12098f99ad4048d82dd
SilentWatcher <vitekarvikas55@gmail.com>
2025-05-21 11:41:54 +0530

    .

---
+++ cho-co/02NUmerianGreetings/numerianGreetings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+input 
+Akash 
+
+
+output 
+
+Hello, Akash
+
+*/
+
+int main() {
+    // Your code starts here
+    string Name;
+    getline(cin, Name);
+    cout <<  "Hello, " <<Name;
+    return 0;
+}
